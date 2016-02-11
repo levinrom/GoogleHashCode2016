6e42bbd7df0015aeeaa8e37a63d434bc305d76ce
RomanLevin <rlevin@shutterfly.com>
2016-02-11 22:33:46 +0200

    step

--- main_manager.php
+++ main_manager.php
@@ -6,6 +6,7 @@ require_once('Product.php');
 require_once('Warehouse.php');
 //require_once('Order.php');
 require_once('src/parser.php');
+require_once('Utils.php');
 
 
 function getClosestWHWithProduct($pId) {
@@ -42,8 +43,32 @@ for ($currStep = 0; $currStep < $world["simLength"]; $currStep++) {
                 foreach($order["deliveries"] as $delivery) {
                     // FROM WHERE?
                     // Get closest WH with the product
-                    if ($drone.addAction('LOAD', $delivery["pId"], $delivery["amount"])) {
-                        $drone.addAction('DELIVER', $delivery["x"], $delivery["y"], $delivery["pId"], $delivery["amount"]);
+                    $possibleWH = [];
+                    foreach($world["warehouses"] as $wh) {
+                        if ($wh->hasProduct($delivery["pId"], $delivery["amount"])) {
+                            $possibleWH[] = $wh;
+                        }
+                    }
+
+                    // can make order
+                    if (count($possibleWH) > 0) {
+
+                        $whIndex = 0;
+                        $minDist = 100000000;
+                        // find closest
+                        foreach($possibleWH as $i => $wh) {
+                            $dist = Utils::distance($wh["x"], $wh["y"], $drone["x"], $drone["y"]);
+                            if ($dist < $minDist) {
+                                $minDist = $dist;
+                                $whIndex = $i;
+                            }
+                        }
+
+                        $chosenWh = $world["warehouses"][$whIndex];
+
+                        if ($drone->addAction('LOAD', $chosenWh["x"], $chosenWh["y"], $delivery["pId"], $delivery["amount"])) {
+                            $drone->addAction('DELIVER', $delivery["x"], $delivery["y"], $delivery["pId"], $delivery["amount"]);
+                        }
                     }
                 }
             }
