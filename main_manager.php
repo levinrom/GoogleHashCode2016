0c26de6ff18270aff4461df174c5aa31784622b8
RomanLevin <rlevin@shutterfly.com>
2016-02-11 21:48:04 +0200

    1

--- main_manager.php
+++ main_manager.php
@@ -1,29 +1,31 @@
 <?php
 
-    //require_once('optimization_strategy1.php');
-    //require_once('drone.php');
-    //require_once('product.php');
-    //require_once('warehouse.php');
-    //require_once('delivery.php');
-    //require_once('parser.php');
+//require_once('optimization_strategy1.php');
+//require_once('drone.php');
+//require_once('product.php');
+//require_once('warehouse.php');
+//require_once('delivery.php');
+//require_once('parser.php');
 
 
 
 
-    $world = [
-        "droneMaxWeight" => 0,
-        "simLength" => 0,
-        "currentTurn" => 0,
-        "rows" => 0,
-        "cols" => 0,
-        "warehouses" => [],
-        "products" => [],
-        "drones" => [],
-        "deliveries" => []
-    ];
+$world = [
+    "droneMaxWeight" => 0,
+    "simLength" => 0,
+    "currentTurn" => 0,
+    "rows" => 0,
+    "cols" => 0,
+    "warehouses" => [],
+    "products" => [],
+    "drones" => [],
+    "deliveries" => []
+];
 
 
-    $world = parser::Parse('input.in');
+$world = parser::Parse('input.in');
 
-    // Event loop
-    for ($currStep = )
+// Event loop
+for ($currStep = 0; $currStep < $world["simLength"]; $currStep++) {
+
+}
