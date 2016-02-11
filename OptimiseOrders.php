88759ad0185e92d4d0e721a38d26c887dfaf77e1
Shimon Cohen <shimonhc@gmail.com>
2016-02-11 21:51:21 +0200

    Optimize Orders

---
+++ OptimiseOrders.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shimon
+ * Date: 2/11/16
+ * Time: 21:26
+ */
+
+
+class OptimiseOrders {
+    public static function sortOrders($warehouses, $orders, $drones) {
+
+        // Give each order a score
+        $orderScores = array();
+        foreach ($orders as $order) {
+            $cOrderScore = 0;
+            foreach ($order->products as $product) {
+                $cOrderScore += self::findClosestWarehouse($product, $order, $warehouses);
+
+            }
+        }
+        // Pick best one
+
+        // Order them by score
+    }
+
+    public static function findClosestWarehouse($product, $order, $warehouses) {
+        $minDist = -1;
+        foreach ($warehouses as $warehouse) {
+            if ($warehouse->has($product)) {
+                $dist = ceil(sqrt(pow($warehouse->x - $order->x, 2) + pow($warehouse->y - $order->y,2)));
+                if ($minDist == -1)
+            }
+        }
+    }
+}
