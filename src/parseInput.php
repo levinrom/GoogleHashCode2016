02ad9c17195301b7e662c545cfb1497933dc995f
igeled <igeled@shutterfly.com>
2016-02-11 21:38:13 +0200

    Ido first commit

---
+++ src/parseInput.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Main
+ * Date: 2/11/16
+ * Time: 21:03
+ */
+
+
+$inputFile = scandir("./Input");
+foreach ($inputFile as $fileName)
+{
+    $file = file("./Input/mother_of_all_warehouses.in");
+}
+//Get the file from command line
+
+
+echo ("1");
+////Read the file
+//$hd = fopen($file, "r");
