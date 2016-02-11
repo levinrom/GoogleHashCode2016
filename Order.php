9af349b54bc2208b6a94a2110bd32254523fe815
noamcb <noamcb@gmail.com>
2016-02-11 22:36:37 +0200

    Order complete

--- Order.php
+++ Order.php
@@ -12,6 +12,7 @@ class Order
     private $y;
     private $array;
     private $oLock;
+    private $id;
 
     function __construct($x, $y, $array)
     {
@@ -19,6 +20,7 @@ class Order
         $this->y = $y;
         $this->array = $array;
         $this->oLock = false;
+        $this->id = $this->gen_uuid();
     }
 
     function getArray()
@@ -42,6 +44,42 @@ class Order
         $this->oLock = false;
     }
 
-    function
+    function updateOrder($array)
+    {
+        $res = false;
+        if ($this->lock()) {
+            $this->array = $array;
+            $this->release();
+            $res = true;
+        }
+        return $res;
+    }
+
+    function getId()
+    {
+        return $this->id;
+    }
+
+    function gen_uuid() {
+        return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+            // 16 bits for "time_mid"
+            mt_rand( 0, 0xffff ),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand( 0, 0x0fff ) | 0x4000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand( 0, 0x3fff ) | 0x8000,
+
+            // 48 bits for "node"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+        );
+    }
 
 }
