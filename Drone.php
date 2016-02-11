897eaf0c5675e72fbface61351aae9afcebb7753
noamcb <noamcb@gmail.com>
2016-02-11 21:25:59 +0200

    Drone constructor

---
+++ Drone.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: noam
+ * Date: 2/11/16
+ * Time: 9:18 PM
+ */
+class Drone
+{
+    private $x;
+    private $y;
+    private $currentPayload;
+    private $productsArray;
+    private $status;
+
+    function __construct($payload, $x, $y)
+    {
+        $this->maxPayload   = $payload;
+        $this->x            = $x;
+        $this->y            = $y;
+        $this->status       = false;
+    }
+
+    
+
+
+}
